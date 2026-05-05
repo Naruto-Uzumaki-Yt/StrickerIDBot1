504efded78cb7fbf2da932c583dc84ce0dfe4305
Naruto-Uzumaki-Yt <narutouzumaki3016day@gmail.com>
2026-05-06 04:19:22 +0530

    utils.py

---
+++ utils.py
@@ -0,0 +1,16 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
+import time
+
+START_TIME = time.time()
+VERSION = "1.0.0"
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
